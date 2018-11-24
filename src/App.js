5992b5ccdb0f59304ce39abf584d25f8fd9e7dea
Hoang Le <lemin2009@gmail.com>
2018-11-24 22:04:55 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
-import Auth from './pages/Auth';
+import LoginPage from './pages/LoginPage';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Link to='/'>goHome</Link>
-          <br/>
-          <Link to='auth'>Login</Link>
-
           <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/auth' component={Auth} />
+            <Route path='/login' component={LoginPage} />
           </Switch>
         </div>
       </BrowserRouter>
