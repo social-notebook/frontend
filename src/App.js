cabe1e41be812bc22d26da5b971f2d5431b121c2
dtanphat9388 <dtanphat9388@gmail.com>
2018-11-24 20:56:12 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './pages/Home';
+import Auth from './pages/Auth';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Link to='/'>goHome</Link>
+          <br/>
+          <Link to='auth'>Login</Link>
+
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/auth' component={Auth} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
